7910cfce711b2f762535306222ac63a3f20e2cc9
LavaDesu <me@lava.moe>
2020-08-24 22:45:10 +0700

    Clean files before build and fix build script

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const { pipeline } = require('readable-stream');
 
 const babel = require("gulp-babel");
+const cleaner = require("gulp-clean");
 const htmlmin = require("gulp-htmlmin");
 const postcss = require("gulp-postcss");
 const uglify = require('gulp-uglify');
@@ -10,25 +11,30 @@ const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 
 const assets = () => pipeline(
-    gulp.src("./src/assets/**"),
+    gulp.src("src/assets/**"),
     gulp.dest("dist/assets")
 );
 
+const clean = () => pipeline(
+    gulp.src("dist", { allowEmpty: true, read: false }),
+    cleaner()
+);
+
 const html = () => pipeline(
-    gulp.src("./src/**/*.html"),
+    gulp.src("src/**/*.html"),
     htmlmin({ collapseWhitespace: true }),
     gulp.dest("dist")
 );
 
 const script = () => pipeline(
-    gulp.src("./src/**/*.js"),
+    gulp.src("src/**/*.js"),
     babel(),
     uglify(),
     gulp.dest("dist")
 );
 
 const style = () => pipeline(
-    gulp.src("./src/**/*.css"),
+    gulp.src("src/**/*.css"),
     postcss([
         autoprefixer({ overrideBrowserslist: ['last 1 version'] }),
         cssnano()
@@ -38,14 +44,15 @@ const style = () => pipeline(
 
 module.exports = {
     assets,
+    clean,
     html,
     script,
     style,
-    default: gulp.parallel(assets, html, script, style),
+    default: gulp.series(clean, gulp.parallel(assets, html, script, style)),
     watch: () => {
-        gulp.watch("./src/assets/**", assets);
-        gulp.watch("./src/**/*.html", html);
-        gulp.watch("./src/**/*.js", script);
-        gulp.watch("./src/**/*.css", style);
+        gulp.watch("src/assets/**", assets);
+        gulp.watch("src/**/*.html", html);
+        gulp.watch("src/**/*.js", script);
+        gulp.watch("src/**/*.css", style);
     }
 };
