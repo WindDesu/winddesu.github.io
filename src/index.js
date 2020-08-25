562cac8152926c2521bbc34c6b70654d66913189
LavaDesu <me@lava.moe>
2020-08-25 15:36:52 +0700

    animation, such wow
    - Use `window.requestAnimationFrame()` (mmm high refresh rate)
    - Matrix merge functions
    - Rainbow particles!
    - Configurable lifespan-opacity
    - Completely seperate global configuration object, with random presets every refresh

--- src/index.js
+++ src/index.js
@@ -1,11 +1,142 @@
+// Main config!
+
+let globalMultiplier = 1;
+
+const presets = [
+    { // index 0
+        name: "Slow",
+        velocity: 0.1,
+        gradient: 2,
+        lifespan: 100,
+        lifespanBase: 50,
+        lifeOpacity: 0.25,
+        opacityMultiplier: 1,
+        size: 3,
+        rainbow: true,
+
+        particles: 100,
+        rate: 50, // lower = faster
+        delayAll: 4000,
+        delayPer: 5
+    },
+    { // index 1
+        name: "Speed",
+        velocity: 0.3,
+        gradient: 10,
+        lifespan: 250,
+        lifespanBase: 450,
+        lifeOpacity: 0.25,
+        opacityMultiplier: 0.8,
+        size: 3,
+        rainbow: true,
+
+        particles: 200,
+        rate: 5, // lower = faster
+        delayAll: 2000,
+        delayPer: 20
+    },
+    { // index 2
+        name: "Speeder",
+        velocity: 0.35,
+        gradient: 20,
+        lifespan: 250,
+        lifespanBase: 200,
+        lifeOpacity: 0.25,
+        opacityMultiplier: 0.8,
+        size: 3,
+        rainbow: true,
+
+        particles: 100,
+        rate: 4, // lower = faster
+        delayAll: 2000,
+        delayPer: 5
+    },
+    { // index 3
+        name: "Speeder mess",
+        velocity: 0.4,
+        gradient: 15,
+        lifespan: 100,
+        lifespanBase: 50,
+        lifeOpacity: 0.25,
+        opacityMultiplier: 1,
+        size: 3,
+        rainbow: true,
+
+        particles: 700,
+        rate: 10, // lower = faster
+        delayAll: 3000,
+        delayPer: 1
+    },
+    { // index 4
+        name: "Stars",
+        velocity: 0.005,
+        gradient: 0,
+        lifespan: 300,
+        lifespanBase: 100,
+        lifeOpacity: 0.1,
+        opacityMultiplier: 0.4,
+        size: 2,
+        rainbow: false,
+
+        particles: 500,
+        rate: 42, // lower = faster
+        delayAll: 5000,
+        delayPer: 10
+    },
+    { // index 5
+        name: "Rainbow stars",
+        velocity: 0.005,
+        gradient: 0,
+        lifespan: 300,
+        lifespanBase: 100,
+        lifeOpacity: 0.1,
+        opacityMultiplier: 0.9,
+        size: 2,
+        rainbow: true,
+
+        particles: 250,
+        rate: 42, // lower = faster
+        delayAll: 5000,
+        delayPer: 10
+    }
+];
+
+const preset = Math.floor(Math.random() * presets.length);
+const config = presets[preset];
+console.log(`preset ${preset} - ${config.name}`);
+
+const concat = (arr1, arr2, cb) => arr1.map((e, i) => cb(e, arr2[i]));
+const merge = (arr1, arr2) => concat(arr1, arr2, (a, b) => a + b);
+
 class Particle {
     constructor() {
         this.started = false;
     }
 
+    get lifespan() {
+        return this._lifespan / globalMultiplier;
+    }
+    set lifespan(value) {
+        return this._lifespan = value;
+    }
+
+    get velocity() {
+        return this._velocity.map(e => e * globalMultiplier);
+    }
+    set velocity(value) {
+        return this._velocity = value;
+    }
+
+    get gradient() {
+        return this._gradient.map(e => e * globalMultiplier);
+    }
+    set gradient(value) {
+        return this._gradient = value;
+    }
+
     start() {
         this.started = true;
-        this.lifespan = Math.random() * 250 + 250;
+        this.lifespan = Math.random() * config.lifespan + config.lifespanBase;
         // this.lifespan = Math.random() * 35 + 35;
         this.age = 0;
         this.initial = [
@@ -14,14 +145,28 @@ class Particle {
         ];
         this.pos = this.initial.slice(0);
         this.velocity = [
-            Math.random() * 0.4 - 0.2,
-            Math.random() * 0.4 - 0.2
+            Math.random() * config.velocity * 2 - config.velocity,
+            Math.random() * config.velocity * 2 - config.velocity
         ];
+        if (config.rainbow) {
+            this.colour = [
+                Math.floor(Math.random() * 256),
+                Math.floor(Math.random() * 256),
+                Math.floor(Math.random() * 256)
+            ];
+            this.gradient = [
+                Math.random() * config.gradient * 2 - config.gradient,
+                Math.random() * config.gradient * 2 - config.gradient,
+                Math.random() * config.gradient * 2 - config.gradient
+            ];
+        } else
+            this.colour = [255, 255, 255];
+
         this.ctx = canvas.getContext("2d");
     }
 
     calcOpacity() {
-        const quarter = this.lifespan / 4;
+        const quarter = this.lifespan * config.lifeOpacity;
         if (this.age < quarter)
             return this.age / quarter;
         else if (this.age > this.lifespan - quarter)
@@ -42,14 +187,13 @@ class Particle {
         if (this.age >= this.lifespan)
             return this.destroy();
 
-        this.pos = [
-            this.pos[0] += this.velocity[0],
-            this.pos[1] += this.velocity[1]
-        ];
+        this.pos = merge(this.pos, this.velocity);
+        if (this.rainbow)
+            this.colour = merge(this.colour, this.gradient);
 
         this.ctx.beginPath();
-        this.ctx.fillStyle = `rgba(255, 255, 255, ${this.calcOpacity() * 0.8})`;
-        this.ctx.arc(this.pos[0], this.pos[1], 3, 0, Math.PI * 2, true);
+        this.ctx.fillStyle = `rgba(${this.colour[0]}, ${this.colour[1]}, ${this.colour[2]}, ${this.calcOpacity() * config.opacityMultiplier})`;
+        this.ctx.arc(this.pos[0], this.pos[1], config.size, 0, Math.PI * 2, true);
         this.ctx.closePath();
         this.ctx.fill();
     }
@@ -58,24 +202,30 @@ class Particle {
 window.particles = [];
 const canvas = document.getElementById("bg");
 
-const amount = 100;
-
 if (canvas.getContext) {
     let i = 0;
-    for (; i <= amount; i++)
+    for (; i < config.particles; i++)
         particles.push(new Particle());
 
     i = 0;
     const init = () => {
-        i++;
-        if (i >= amount)
+        if (i >= config.particles)
             return;
         particles[i].start();
-        setTimeout(init, Math.random() * 10 + 5000 / amount);
+        setTimeout(init, Math.random() * config.delayPer + config.delayAll / config.particles);
+        i++;
     };
-    init();
+    setTimeout(init, 450);
 
-    setInterval(() => {
+    let prev = 0;
+    const render = t => {
+        if (t < 500) {
+            globalMultiplier = (t - prev) / config.rate;
+            prev = t;
+            window.requestAnimationFrame(render);
+            return;
+        }
+
         if (canvas.clientHeight !== window.innerHeight)
             canvas.setAttribute("height", window.innerHeight);
         if (canvas.clientWidth !== window.innerWidth)
@@ -83,5 +233,9 @@ if (canvas.getContext) {
 
         canvas.getContext("2d").clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
         particles.forEach(part => part.draw());
-    }, 15);
+
+        window.requestAnimationFrame(render);
+    };
+
+    render();
 }
